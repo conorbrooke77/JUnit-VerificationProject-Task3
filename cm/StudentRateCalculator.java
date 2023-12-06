f86e6085524f54d4e5f380130afa3ee26e336d83
conorbrooke77 <conorbrooke2018@gmail.com>
2023-12-06 19:01:13 +0000

    Modifying code to follow updated specification for Kind.Student to allow test case rateCalculateTestCase13() to pass.

--- cm/StudentRateCalculator.java
+++ cm/StudentRateCalculator.java
@@ -1,8 +1,24 @@
 import java.math.BigDecimal;
 
 public class StudentRateCalculator implements ICalculateParkingRate {
+    private final BigDecimal LIMIT_BEFORE_REDUCTION = new BigDecimal("5.5");
+    private final BigDecimal REDUCTION_RATE = new BigDecimal("0.33"); // 33%
+
     @Override
     public BigDecimal calculateRate(BigDecimal calculatedRate) {
-        return null;
+        // Check if the calculated rate exceeds the Limit
+        if (calculatedRate.compareTo(LIMIT_BEFORE_REDUCTION) > 0) {
+            // Calculate the amount after the limit
+            BigDecimal aboveLimit = calculatedRate.subtract(LIMIT_BEFORE_REDUCTION);
+
+            // Apply a 33% reduction
+            BigDecimal reducedAmount = aboveLimit.multiply(REDUCTION_RATE);
+
+            // Subtract the reduced amount from the excess amount and add the limit
+            return calculatedRate.subtract(reducedAmount);
+        } else {
+            // If the calculated rate is below the limit, return it as is
+            return calculatedRate;
+        }
     }
 }
